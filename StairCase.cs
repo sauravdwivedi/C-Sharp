4832c986e9a4b4575ccaa28499b33f5e5593562d
sauravdwivedi <saurav.dwivedi@gmail.com>
2022-04-24 12:03:06 +0200

    Modify StairCase.cs

--- StairCase.cs
+++ StairCase.cs
@@ -10,33 +10,24 @@
  *  a staircase for a given integer n as input.
  */
  
- 
 using System;
 using System.IO;
 
-
 namespace StairCaseApplication {
   class StairCase {
-  
     public string PrintStairCase(int stairCaseSize) {
       string stairCase = "";
-        
       for (int i = 1; i < stairCaseSize + 1; i++) {
-      
         for (int j = 0; j < stairCaseSize - i; j++) {
           stairCase += " ";
         }
-          
         for (int k = 0; k < i; k++) {
           stairCase += "#";
         }
-          
         stairCase += "\n";
       }
-        
       return stairCase;
     }
-      
       
     public static void Main(string[] args) {
       StairCase obj = new StairCase();
